f5d0f1a9113651c15dc90544d85410d91a960aa2
patrickliu022 <patrickliu022@gmail.com>
2022-04-20 21:28:51 -0700

    added -1 button for player one

--- lifecounter/lifecounter/ViewController.swift
+++ lifecounter/lifecounter/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playerOneLife: UILabel!
+    var playerOneTotalLife = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func playerOneMinusOne(_ sender: Any) {
+        playerOneTotalLife = playerOneTotalLife - 1
+        playerOneLife.text = "\(playerOneTotalLife)"
+    }
+    
+    @IBAction func playerOnePlusOne(_ sender: Any) {
+        playerOneTotalLife = playerOneTotalLife + 1
+        playerOneLife.text = "\(playerOneTotalLife)"
+    }
 }
 
