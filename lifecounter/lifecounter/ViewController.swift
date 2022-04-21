95346e390233d355a5f5c540c8c980334b3b9f99
patrickliu022 <patrickliu022@gmail.com>
2022-04-20 23:37:36 -0700

    implemented counter logic

--- lifecounter/lifecounter/ViewController.swift
+++ lifecounter/lifecounter/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var playerOneLife: UILabel!
     @IBOutlet weak var playerTwoLife: UILabel!
+    @IBOutlet weak var playerXLost: UILabel!
+    @IBOutlet weak var resetGameButton: UIButton!
     
     var playerOneTotalLife = 20
     var playerTwoTotalLife = 20
@@ -18,11 +20,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        playerXLost.isHidden = true
+        resetGameButton.isHidden = true
     }
 
 
     @IBAction func playerOneMinusOne(_ sender: Any) {
         playerOneTotalLife = playerOneTotalLife - 1
+        checkPlayerStillHasLife(playerOneTotalLife, "1")
         playerOneLife.text = "\(playerOneTotalLife)"
     }
     
@@ -33,6 +38,7 @@ class ViewController: UIViewController {
     
     @IBAction func playerOneMinusFive(_ sender: Any) {
         playerOneTotalLife = playerOneTotalLife - 5
+        checkPlayerStillHasLife(playerOneTotalLife, "1")
         playerOneLife.text = "\(playerOneTotalLife)"
     }
     
@@ -43,6 +49,7 @@ class ViewController: UIViewController {
     
     @IBAction func playerTwoMinusOne(_ sender: Any) {
         playerTwoTotalLife = playerTwoTotalLife - 1
+        checkPlayerStillHasLife(playerTwoTotalLife, "2")
         playerTwoLife.text = "\(playerTwoTotalLife)"
     }
     
@@ -53,12 +60,31 @@ class ViewController: UIViewController {
     
     @IBAction func playerTwoMinusFive(_ sender: Any) {
         playerTwoTotalLife = playerTwoTotalLife - 5
+        checkPlayerStillHasLife(playerTwoTotalLife, "2")
         playerTwoLife.text = "\(playerTwoTotalLife)"
     }
     
     @IBAction func playerTwoPlusFive(_ sender: Any) {
         playerTwoTotalLife = playerTwoTotalLife + 5
         playerTwoLife.text = "\(playerTwoTotalLife)"
+    }
+    
+    
+    @IBAction func resetGame(_ sender: Any) {
+        playerXLost.isHidden = true
+        resetGameButton.isHidden = true
+        playerTwoLife.text = "20"
+        playerTwoTotalLife = 20
+        playerOneLife.text = "20"
+        playerOneTotalLife = 20
+    }
+    
+    func checkPlayerStillHasLife(_ lifeNumber : Int, _ player: String) {
+        if (lifeNumber <= 0) {
+            playerXLost.isHidden = false
+            resetGameButton.isHidden = false
+            playerXLost.text = "Player \(player) LOSES!"
+        }
     }
     
 }
