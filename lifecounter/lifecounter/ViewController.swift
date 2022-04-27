b016fd575e823ac24ae65dc4ebdde05a9c9b8103
patrickliu022 <patrickliu022@gmail.com>
2022-04-26 23:27:15 -0700

    created temporary table view

--- lifecounter/lifecounter/ViewController.swift
+++ lifecounter/lifecounter/ViewController.swift
@@ -9,83 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var playerOneLife: UILabel!
-    @IBOutlet weak var playerTwoLife: UILabel!
-    @IBOutlet weak var playerXLost: UILabel!
-    @IBOutlet weak var resetGameButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
     
-    var playerOneTotalLife = 20
-    var playerTwoTotalLife = 20
+    let names = [
+        "John Smith",
+        "Dan Smith",
+        "Jason Smith",
+        "Mary Smith"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        playerXLost.isHidden = true
-        resetGameButton.isHidden = true
-    }
-
-
-    @IBAction func playerOneMinusOne(_ sender: Any) {
-        playerOneTotalLife = playerOneTotalLife - 1
-        checkPlayerStillHasLife(playerOneTotalLife, "1")
-        playerOneLife.text = "\(playerOneTotalLife)"
-    }
-    
-    @IBAction func playerOnePlusOne(_ sender: Any) {
-        playerOneTotalLife = playerOneTotalLife + 1
-        playerOneLife.text = "\(playerOneTotalLife)"
-    }
-    
-    @IBAction func playerOneMinusFive(_ sender: Any) {
-        playerOneTotalLife = playerOneTotalLife - 5
-        checkPlayerStillHasLife(playerOneTotalLife, "1")
-        playerOneLife.text = "\(playerOneTotalLife)"
-    }
-    
-    @IBAction func playerOnePlusFive(_ sender: Any) {
-        playerOneTotalLife = playerOneTotalLife + 5
-        playerOneLife.text = "\(playerOneTotalLife)"
-    }
-    
-    @IBAction func playerTwoMinusOne(_ sender: Any) {
-        playerTwoTotalLife = playerTwoTotalLife - 1
-        checkPlayerStillHasLife(playerTwoTotalLife, "2")
-        playerTwoLife.text = "\(playerTwoTotalLife)"
-    }
-    
-    @IBAction func playerTwoPlusOne(_ sender: Any) {
-        playerTwoTotalLife = playerTwoTotalLife + 1
-        playerTwoLife.text = "\(playerTwoTotalLife)"
-    }
-    
-    @IBAction func playerTwoMinusFive(_ sender: Any) {
-        playerTwoTotalLife = playerTwoTotalLife - 5
-        checkPlayerStillHasLife(playerTwoTotalLife, "2")
-        playerTwoLife.text = "\(playerTwoTotalLife)"
-    }
-    
-    @IBAction func playerTwoPlusFive(_ sender: Any) {
-        playerTwoTotalLife = playerTwoTotalLife + 5
-        playerTwoLife.text = "\(playerTwoTotalLife)"
-    }
-    
-    
-    @IBAction func resetGame(_ sender: Any) {
-        playerXLost.isHidden = true
-        resetGameButton.isHidden = true
-        playerTwoLife.text = "20"
-        playerTwoTotalLife = 20
-        playerOneLife.text = "20"
-        playerOneTotalLife = 20
-    }
-    
-    func checkPlayerStillHasLife(_ lifeNumber : Int, _ player: String) {
-        if (lifeNumber <= 0) {
-            playerXLost.isHidden = false
-            resetGameButton.isHidden = false
-            playerXLost.text = "Player \(player) LOSES!"
-        }
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
 }
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("you tapped me")
+    }
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+}
+
 
